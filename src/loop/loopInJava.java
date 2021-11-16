4dac8c0094ff2b89f9ed96f0bcfb790b141d63f4
Mervel999 <manishjobs1999@gmail.com>
2021-11-17 02:29:09 +0530

    For loop

--- src/loop/loopInJava.java
+++ src/loop/loopInJava.java
@@ -1,4 +1,11 @@
 package loop;
 
 public class loopInJava {
+    public static void main(String[] args) {
+        for (int i=0; i<10;i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println(i + " : " + j);
+            }
+        }
+    }
 }
