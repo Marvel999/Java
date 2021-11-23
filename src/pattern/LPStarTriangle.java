0886cd6b6e5903e36ca356ee50e00846a218f212
Mervel999 <manishjobs1999@gmail.com>
2021-11-23 05:36:09 +0530

    Pattern

---
+++ src/pattern/LPStarTriangle.java
@@ -0,0 +1,19 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class LPStarTriangle {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        for (int y=1; y<=n;y++){
+            for (int x=1; x<=n; x++){
+                if(x<=y && x>=1 && y<=n)
+                System.out.print("* ");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
