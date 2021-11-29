8b608c078167db1936bb8f7ab19f3e0787c11bf0
Mervel999 <manishjobs1999@gmail.com>
2021-11-30 03:00:21 +0530

    Pattern

---
+++ src/pattern/equestion/Pattern10.java
@@ -0,0 +1,19 @@
+package pattern.equestion;
+
+import java.util.Scanner;
+
+public class Pattern10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        for (int c=0; c<n; c++){
+            for( int r=0; r<n; r++){
+                if(c+r==((n-1)/2) || r-c==((n-1)/2) || c-r==((n-1)/2) || c+r==(3*(n-1)/2)){
+                    System.out.print("*\t");
+                }else
+                    System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+}
