57dd7e96bd980f73fb923c505360616f849a90bd
Mervel999 <manishjobs1999@gmail.com>
2021-11-30 02:42:22 +0530

    Pattern

--- src/pattern/equestion/Pattern5.java
+++ src/pattern/equestion/Pattern5.java
@@ -6,12 +6,12 @@ public class Pattern5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
-        for (int c=0; c<=n; c++){
-            for( int r=0; r<=n; r++){
-                if(c+r<(n+3)/2 || c-r>(n-1)/2 || c+r>(3*n+1)/2 || r-c>(n-1)/2){
-                    System.out.print("\t");
-                }else
+        for (int c=0; c<n; c++){
+            for( int r=0; r<n; r++){
+                if(c+r>=((n-1)/2) && r-c<=((n-1)/2) && c-r<=((n-1)/2) && c+r<=(3*(n-1)/2)){
                     System.out.print("*\t");
+                }else
+                    System.out.print("\t");
             }
             System.out.println();
         }
