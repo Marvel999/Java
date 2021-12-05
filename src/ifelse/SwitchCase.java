68695a02ab332db27a62894416cdce7989a7d753
Mervel999 <manishjobs1999@gmail.com>
2021-12-06 00:20:48 +0530

    Switch case

---
+++ src/ifelse/SwitchCase.java
@@ -0,0 +1,18 @@
+package ifelse;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        int num= 35;
+        switch (num){
+            case 20 :
+                System.out.println("20");
+                break;
+            case 30 :
+                System.out.println("30");
+                break;
+            default:
+                System.out.println("num is not 30 & 20");
+
+        }
+    }
+}
